aa42ce5be38665f8f9d6ecd9e88ffa8566258834
Tommy Terry <tommyterry2005@gmail.com>
2022-06-15 13:43:44 -0500

    Volume of box

---
+++ VolumeOfABox.js
@@ -0,0 +1,6 @@
+let legnth = parseInt(prompt("Please legnth"));
+let width = parseInt(prompt("Please width"));
+let height = parseInt(prompt("Please height"));
+let volume = legnth * width * height;
+console.log("The volume is " + volume);
+
