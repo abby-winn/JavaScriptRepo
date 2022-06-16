e7e43fd988085e0a320d9cf7a3838125bddbe42f
Tommy Terry <tommyterry2005@gmail.com>
2022-06-16 09:41:06 -0500

    Fixed a couple things

--- LongestLastName.js
+++ LongestLastName.js
@@ -2,29 +2,36 @@ let peopleKnown = parseInt(prompt("How many people do you know?"));
 
 const people = [];
 for(let i = 0; i < peopleKnown; i++){
-    let person = prompt("What is a person's name?");
+    let person = prompt("What is a person full's name?");
     people.push(person);
 }
 
-let longName = people[0];
+let lastName = people[0].substring(people[0].indexOf(" ") + 1);
 for(let i = 1; i < people.length; i++){
-    if(longName.length < people[i].length){
-        longName = people[i];
+    let currentLastName = people[i].substring(people[i].indexOf(" ") + 1);
+
+    if(currentLastName.length > lastName.length){
+        lastName = currentLastName;
     }
 }
 
-console.log("The longest name is " + longName);
+console.log("The longest last name is " + lastName);
 
-let shortName = people[0];
+let shortLength = people[0].length;
+let longLength = people[0].length;
 for(let i = 1; i < people.length; i++){
-    if(shortName.length > people[i].length){
-        shortName = people[i];
+    if(people[i].length < shortLength){
+        shortLength = people[i].length;
+    }
+    if(people[i].length > longLength){
+        longLength = people[i].length;
     }
 }
 
-console.log("The range of the name length is " + shortName.length + " to " + longName.length);
+console.log("The range is " + (shortLength - 1) + " to " + (longLength - 1));
 
 
+console.log("All the people are");
 for(let i = 0; i < people.length; i++){
     console.log(people[i]);
 }
